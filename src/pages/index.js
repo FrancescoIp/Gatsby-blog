9f96719e418dc7acf678e7621949ad0067c64645
Francesco Ippolito <ipp.francesco@gmail.com>
2021-04-21 13:06:29 +0100

    remove link not used

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from 'gatsby'
+
 import Layout from '../components/layout'
 import Head from '../components/head'
 
@@ -7,7 +7,7 @@ const IndexPage = () => {
   return (
     <Layout>
       <Head title="Home"/>
-      <h1>Hello world!</h1>
+      <h1>Hello GUYS</h1>
       <h2>Pezzo de merda</h2>
     </Layout>
   )
